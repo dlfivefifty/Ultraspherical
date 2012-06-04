bfde50fbe572e9df150f3a669972eccc2947ae43
Sheehan Olver <Sheehan.Olver@sydney.edu.au>
2012-06-05 08:45:13 +1000

    Try using uBLAS Banded Matrix

--- Ultraspherical/main.cpp
+++ Ultraspherical/main.cpp
@@ -7,12 +7,28 @@
 //
 
 #include <iostream>
+#include <boost/numeric/ublas/banded.hpp>
+#include <boost/numeric/ublas/io.hpp>
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    using namespace boost::numeric::ublas;
+//    banded_matrix<double> m (6, 6, 1, 1);
+//    for (signed i = 0; i < signed (m.size1 ()); ++ i)
+//        for (signed j = std::max (i - 1, 0); j < std::min (i + 2, signed (m.size2 ())); ++ j)
+//            m (i, j) = 3 * i + j;
+//    std::cout << m << std::endl;
+//    std::cout << m.lower() << std::endl;
+//    std::cout << m.upper() << std::endl;
+    
+    banded_matrix<double> m (6, 6);
+    
+    m.resize(6,6);
+    m(3,3) = 3;
+    
+    std::cout << m(3,3) << std::endl;
+
     return 0;
 }
 
