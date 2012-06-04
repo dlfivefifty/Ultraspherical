6095ac1ea91a0a5e9fd8882c37ec289799912851
Sheehan Olver <Sheehan.Olver@sydney.edu.au>
2012-06-04 21:03:13 +1000

    Started Files

---
+++ Ultraspherical/BandedMatrix.cpp
@@ -0,0 +1,9 @@
+//
+//  BandedMatrix.cpp
+//  Ultraspherical
+//
+//  Created by Sheehan Olver on 04/06/2012.
+//  Copyright (c) 2012 School of Mathematics and Statistics, The University of Sydney. All rights reserved.
+//
+
+#include <iostream>
---
+++ Ultraspherical/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Ultraspherical
+//
+//  Created by Sheehan Olver on 04/06/2012.
+//  Copyright (c) 2012 School of Mathematics and Statistics, The University of Sydney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
