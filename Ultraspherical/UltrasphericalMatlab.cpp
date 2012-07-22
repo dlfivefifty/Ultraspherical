d2b7d8dbb6e2095eec519bce60ad95ea6b08ec69
Sheehan Olver <Sheehan.Olver@sydney.edu.au>
2012-07-22 22:14:18 +1000

    Started matlab mex command

---
+++ Ultraspherical/UltrasphericalMatlab.cpp
@@ -0,0 +1,49 @@
+//
+//  Matlab.cpp
+//  Ultraspherical
+//
+//  Created by Sheehan Olver on 22/07/2012.
+//  Copyright (c) 2012 School of Mathematics and Statistics, The University of Sydney. All rights reserved.
+//
+
+//#include <iostream>
+
+#include "mex.h"
+
+// uncomment below and compile with mex main.cpp to generate MATLAB interface.
+
+void mexFunction ( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    
+    int argc; argc = 1; 
+    const char * argv[1]; 
+    //vector<double>* c; 
+    
+    //run main. 
+    // main(argc,argv);
+    
+    //First entry is lower row, it's important!
+//    FilledBandedMatrix bnd(-2,&D2Dirichlet);
+//    bnd.increaseSize();
+//    bnd.increaseSize();
+//    bnd.increaseSize();    
+//    bnd.increaseSize();    
+//    bnd.increaseSize(); 
+//    bnd.increaseSize(); 
+//    vector<double> b,c;
+//    
+//    b.push_back(1);
+//    for(long i = 0; i < 50000; i++)
+//        b.push_back(1);
+//    
+//    c = QRSolve(bnd,b);
+    
+    // return the coefficients of the solution. 
+    const int len = (int) 1; 
+    plhs[0] = mxCreateDoubleMatrix(len,1,mxREAL); // real coeffs vector. 
+    double * y = mxGetPr(plhs[0]); 
+    
+    // assign coeff vector to output. 
+    for( int i = 0; i<len; ++i)
+        y[i] = 2;  
+}
--- Ultraspherical/main.cpp
+++ Ultraspherical/main.cpp
@@ -7,11 +7,10 @@
 //
 
 
-//#include "FilledBandedMatrix.h"
-#include "FilledBandedMatrix.h" // stop multiple definition of RowFiller class. 
+
+#include "FilledBandedMatrix.h" 
 #include "MultiplicationMatrices.h"
 
-//#include "mex.h"
 #include "AdaptiveQR.h"
 #include <time.h>
 #include <math.h>
@@ -164,42 +163,5 @@ int main(int argc, const char * argv[])
     return 0;
 }
 
-// uncomment below and compile with mex main.cpp to generate MATLAB interface.
-/*
-void mexFunction ( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
-{
 
-int argc; argc = 1; 
-const char * argv[1]; 
-//vector<double>* c; 
-
-//run main. 
-main(argc,argv);
-
-    //First entry is lower row, it's important!
-    FilledBandedMatrix bnd(-2,&D2Dirichlet);
-        bnd.increaseSize();
-        bnd.increaseSize();
-        bnd.increaseSize();    
-        bnd.increaseSize();    
-        bnd.increaseSize(); 
-        bnd.increaseSize(); 
-    vector<double> b,c;
-
-    b.push_back(1);
-    for(long i = 0; i < 50000; i++)
-        b.push_back(1);
-    
-    c = QRSolve(bnd,b);
-
-// return the coefficients of the solution. 
-const int len = (int)c.size(); 
-plhs[0] = mxCreateDoubleMatrix(len,1,mxREAL); // real coeffs vector. 
-double * y = mxGetPr(plhs[0]); 
-
-// assign coeff vector to output. 
-for( int i = 0; i<len; ++i)
-    y[i] = c[i];  
-}
-*/
 
